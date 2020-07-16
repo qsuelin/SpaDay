a1c4e4bdd3909c0b889e66e9942485d7f0099fcc
qsuelin <66089958+qsuelin@users.noreply.github.com>
2020-07-16 16:35:26 -0500

    no bonus

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController: Controller
+    {
+        public UserController()
+        {
+        }
+
+        [HttpGet]
+        [Route("/user/add")]
+        public IActionResult Add()
+        {
+            ViewBag.error = false;
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (newUser.Password == verify)
+            {
+                ViewBag.error = false;
+                ViewBag.userName = newUser.Username;
+                ViewBag.email = newUser.Email;
+                return View("Index");
+            }
+            ViewBag.error = true;
+            ViewBag.userName = newUser.Username;
+            ViewBag.email = newUser.Email;
+            return View("Add");
+        }
+
+
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public User()
+        {
+        }
+    }
+}
